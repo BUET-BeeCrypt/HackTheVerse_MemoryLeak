f6dd8d09f15028a143ed0f2c418fb7f7302e7b6d
Tamim Ehsan <tamimehsan99@gmail.com>
2020-11-15 00:00:17 +0600

    Added Bluetooth activty to connect with internet

--- ControllerApp/app/src/main/java/com/engliplan/telelabclient/MainActivity.java
+++ ControllerApp/app/src/main/java/com/engliplan/telelabclient/MainActivity.java
@@ -2,17 +2,32 @@ package com.engliplan.telelabclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button connectBTButton;
     private Client client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Bluetooth bluetooth = Bluetooth.getInstance();
+        bluetooth.setContext(this);
+        connectBTButton = (Button) findViewById(R.id.connectBTButton);
+        connectBTButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ConnectBTActivity.class);
+                startActivity(intent);
+            }
+        });
 
         client=Client.getInstance();
         client.addOnCommandReceivedListener(new Client.OnCommandRecievedListener() {
